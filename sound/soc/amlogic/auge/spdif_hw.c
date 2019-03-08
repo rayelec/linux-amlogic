3e853a0e402ef5de80e6de4aef4cdd491de2d36a
Arthur Liberman <arthur_liberman@hotmail.com>
2019-03-09 00:58:19 +0200

    sound/soc/auge: fix PCM after stopping playback of passthrough audio

--- sound/soc/amlogic/auge/spdif_hw.c
+++ sound/soc/amlogic/auge/spdif_hw.c
@@ -650,6 +650,8 @@ void spdifout_play_with_zerodata(unsigned int spdif_id, bool reenable)
 		struct iec958_chsts chsts;
 		struct snd_pcm_substream substream;
 		struct snd_pcm_runtime runtime;
+		extern unsigned int IEC958_mode_codec;
+		IEC958_mode_codec = 0;
 
 		substream.runtime = &runtime;
 		runtime.rate = sample_rate;
